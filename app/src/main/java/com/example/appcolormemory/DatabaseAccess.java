62fe4d8e1bb632087dcf486df59e0612e440f4f1
hashgrem <jeremydemard@gmail.com>
2021-03-09 19:40:28 +0100

    insertion champs Nom, Email et Password dans la bd

--- app/src/main/java/com/example/appcolormemory/DatabaseAccess.java
+++ app/src/main/java/com/example/appcolormemory/DatabaseAccess.java
@@ -32,15 +32,45 @@ public class DatabaseAccess {
         }
     }
 
-
-
     //requetes ici.
+
+    //Requetes insertion des champs entrés par l'utilisateur dans la table users.
 
     public String addFirstName(String firstNameUser){
 
         c = db.rawQuery("INSERT INTO users (firstName) values(?)", new String[]{firstNameUser});
         c.moveToFirst();
-        return c.getString(0);
+        return c.getString(1); //colonne dans la bd ?
+    }
+
+    public String addLastName(String lastNameUser){
+        c = db.rawQuery("INSERT INTO users (lastName) values(?)", new String[]{lastNameUser});
+        c.moveToFirst();
+        return c.getString(2);
+    }
+
+    public String addEmail(String emailUser){
+        c = db.rawQuery("INSERT INTO users (email) values(?)", new String[]{emailUser});
+        c.moveToFirst();
+        return c.getString(5);
+    }
+
+    public String addPassword(String passwordUser){
+        c = db.rawQuery("INSERT INTO users (password) values (?)", new String[]{passwordUser});
+        c.moveToFirst();
+        return c.getString(6);
+    }
+
+    public String addSexe(String sexeUser){             // --------------------- Radio bouton donc pas un String ------------------------//
+        c = db.rawQuery("INSERT INTO users (sexe) values (?)", new String[]{sexeUser});
+        c.moveToFirst();
+        return c.getString(3);
+    }
+
+    public String addBirthDate(String birthDateUser){   //pareil que pour le sexe
+        c = db.rawQuery("INSERT INTO users (birthDate) values (?)", new String[]{birthDateUser});
+        c.moveToFirst();
+        return c.getString(4);
     }
 }
 
--- app/src/main/java/com/example/appcolormemory/Login.java
+++ app/src/main/java/com/example/appcolormemory/Login.java
@@ -9,6 +9,9 @@ public class Login extends AppCompatActivity {
 
     //Variables de classe:
     private String prenomUtilisateur = "";
+    private String nomUtilisateur = "";
+    private String emailUtilisateur = "";
+    private String passwordUtilisateur = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,9 +23,28 @@ public class Login extends AppCompatActivity {
 
         //On récupère la valeur de chaque champs du formulaire.
 
+        //Prénom
         EditText monEditTextFirstName = (EditText)findViewById(R.id.first_name);
         String monPrenom =  monEditTextFirstName.getText().toString();
         prenomUtilisateur = db.addFirstName(monPrenom);
+
+        //Nom
+        EditText monEditTextLastName = (EditText)findViewById(R.id.last_name);
+        String monNomDeFamille = monEditTextLastName.getText().toString();
+        nomUtilisateur = db.addLastName(monNomDeFamille);
+
+        //Email
+        EditText monEditTextEmail = (EditText)findViewById(R.id.email);
+        String monEmail = monEditTextEmail.getText().toString();
+        emailUtilisateur = db.addEmail(monEmail);
+
+        //Mot de passe
+        EditText monEditTextPassword = (EditText)findViewById(R.id.password);
+        String monPassword = monEditTextPassword.getText().toString();
+        passwordUtilisateur = db.addPassword(monPassword);
+
+
+
 
 
 
