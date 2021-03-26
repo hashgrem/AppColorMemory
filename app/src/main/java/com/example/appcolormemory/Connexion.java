bf3f5f844cd7ab6b5a7cbaa7623bd906c409ba1b
unset <mathieu.bourlioux@gmail.com>
2021-03-27 00:10:45 +0100

    Ajout base de donnée (inscription et connexion fontionnelles

--- app/src/main/java/com/example/appcolormemory/Connexion.java
+++ app/src/main/java/com/example/appcolormemory/Connexion.java
@@ -2,13 +2,44 @@ package com.example.appcolormemory;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Connexion extends AppCompatActivity {
+
+    EditText  email,password;
+    Button btn_connexion;
+    SQLiteHelper db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_connexion);
+
+
+        email = findViewById(R.id.connexion_email);
+        password = findViewById(R.id.connexion_password);
+
+        btn_connexion = findViewById(R.id.connexion_btn_connexion);
+
+        db=new SQLiteHelper(getApplicationContext());
+
+        btn_connexion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (db.connection(email.getText().toString(),password.getText().toString())){
+                    Intent intent = new Intent(getApplicationContext(), MainMenu.class);
+                    startActivity((intent));
+                    finish();
+                }
+                else{
+                    Toast.makeText(Connexion.this, "Erreur de connexion", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/appcolormemory/MainMenu.java
+++ app/src/main/java/com/example/appcolormemory/MainMenu.java
@@ -6,11 +6,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainMenu extends AppCompatActivity {
 
     private Button btn_jouer;
     private Button btn_profil;
+
+    SQLiteHelper db;
+    TextView username_msg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +22,21 @@ public class MainMenu extends AppCompatActivity {
         setContentView(R.layout.layout_main_menu);
 
         this.btn_jouer = (Button) findViewById(R.id.btn_jouer);
+        username_msg = (TextView) findViewById(R.id.main_menu_username_msg);
+
+        db=new SQLiteHelper(getApplicationContext());
+
+        
+
+
+
+
+
+
+
+
+
+
 
         btn_jouer.setOnClickListener(new View.OnClickListener() {
             @Override
